2f21e4b5896485f4d1b027e13e451a2fa04d8d7b
Rakib <rakibpro98@gmail.com>
2023-05-27 23:38:34 +0600

    banner added on hero section

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -6,9 +6,9 @@ import Footer from '../Pages/Shared/Footer/Footer';
 const Main = () => {
     return (
         <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            <Navbar />
+            <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './Banner.css';
+import chair from '../../../assets/images/chair.png';
 
 const Banner = () => {
-    return <div></div>;
+    return (
+        <div className='banner-container hero'>
+            <div className='hero-content flex-col lg:flex-row-reverse'>
+                <img src={chair} className='rounded-lg w-1/2 shadow-2xl' />
+                <div>
+                    <h1 className='text-5xl font-bold leading-snug	'>
+                        Your New Smile Starts Here
+                    </h1>
+                    <p className='py-6'>
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                        assumenda excepturi exercitationem quasi. In deleniti
+                        eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className='btn btn-primary text-white border-none bg-gradient-to-r from-primary to-secondary'>
+                        Get Started
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Banner;
