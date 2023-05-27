5c9d22ddd8a4623f2e92c0237bb6ef061db35f5d
Rakib <rakibpro98@gmail.com>
2023-05-27 17:17:11 +0600

    routes added and home,navbar,footer and some component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes/Routes';
 
 function App() {
     return (
         <div className='App'>
-            <button className='btn'>Button</button>
+            <RouterProvider router={router} />
         </div>
     );
 }
