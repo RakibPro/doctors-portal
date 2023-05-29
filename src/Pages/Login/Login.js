27037b19953c72b37222da7cc96097dcd1c1f595
Rakib <rakibpro98@gmail.com>
2023-05-29 21:31:53 +0600

    login form added and firebase added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState('');
+
+    const handleLogin = (data) => {
+        console.log(data);
+    };
     return (
-        <div>
-            <h2>This is Login</h2>
-        </div>
+        <section className='h-[600px] flex justify-center items-center'>
+            <div
+                className='w-96 p-8'
+                style={{
+                    boxShadow: '3px 4px 10px 2px rgba(0, 0, 0, 0.05)',
+                    borderRadius: '18px',
+                }}
+            >
+                <h2 className='text-4xl text-center text-black'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className='form-control w-full max-w-xs'>
+                        <label className='label'>
+                            <span className='label-text text-black'>Email</span>
+                        </label>
+                        <input
+                            type='email'
+                            className='input input-bordered w-full max-w-xs'
+                            {...register('email')}
+                        />
+                        <label className='label'>
+                            <span className='label-text text-black'>
+                                Password
+                            </span>
+                        </label>
+                        <input
+                            type='password'
+                            className='input input-bordered w-full max-w-xs'
+                            {...register('password')}
+                        />
+                        <label className='label mb-3'>
+                            <Link to='/' className='label-text-alt text-black'>
+                                Forgot Password ?
+                            </Link>
+                        </label>
+                    </div>
+                    <input
+                        type='submit'
+                        value='Login'
+                        className='btn btn-accent w-full max-w-xs font-normal text-[#D4D9E3]'
+                    />
+                    <p className='font-normal text-black text-center text-sm my-4'>
+                        New to Doctors Portal?
+                        <Link to='/' className='text-secondary'>
+                            {' '}
+                            Create new account
+                        </Link>
+                    </p>
+                    <div className='divider text-black'>OR</div>
+                    <button className='btn btn-accent btn-outline w-full max-w-xs font-normal'>
+                        CONTINUE WITH GOOGLE
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 };
 
